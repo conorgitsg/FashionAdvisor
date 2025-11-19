5736cc3b08784e54d2b39789b6efa049f0d7c1ed
roengel-8 <kitkatman94@gmail.com>
2025-11-19 10:53:54 +0800

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,10 +5,23 @@ export const routes: Routes = [
     path: 's3-test',
     loadComponent: () => import('./components/s3-test/s3-test.component').then(m => m.S3TestComponent)
   }
+  
   {
   path: 'camera',
   loadComponent: () =>
     import('./camera/camera-capture.component').then(
       (m) => m.CameraCaptureComponent
     ),
-}];
+}
+
+{
+  path: 'upload',
+  loadComponent: () =>
+    import('./upload/upload-page.component').then(
+      (m) => m.UploadPageComponent
+    ),
+},
+
+
+
+];
