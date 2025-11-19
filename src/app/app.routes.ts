63ad4e884b94ff55c0b873f32a0e05bf53661591
BoraSora <37449694+LookUp11@users.noreply.github.com>
2025-11-19 10:20:35 +0800

    Update camera path app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,4 +5,10 @@ export const routes: Routes = [
     path: 's3-test',
     loadComponent: () => import('./components/s3-test/s3-test.component').then(m => m.S3TestComponent)
   }
-];
+  {
+  path: 'camera',
+  loadComponent: () =>
+    import('./camera/camera-capture.component').then(
+      (m) => m.CameraCaptureComponent
+    ),
+}];
